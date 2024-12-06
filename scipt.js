dc4dc89c8df5f8d0e2940e06e91a79dc44687439
Shuvam BC <81623225+bcShuvam@users.noreply.github.com>
2024-12-06 22:23:57 +0545

    First Commit

--- scipt.js
+++ scipt.js
@@ -189,15 +189,15 @@ function getTotalPrice(groceries) {
   }
   return `Newt Amount = ${newAmt}`;
 }
-console.log(getTotalPrice([{ product: "Milk", quantity: 1, price: 1.5 }])); // Output: Newt Amount = 1.5
-console.log(
-  getTotalPrice([
-    { product: "Milk", quantity: 1, price: 1.5 },
-    { product: "Eggs", quantity: 12, price: 0.1 },
-    { product: "Bread", quantity: 2, price: 1.6 },
-    { product: "Cheese", quantity: 1, price: 4.5 },
-  ])
-); // Output: Newt Amount = 10.4
+// console.log(getTotalPrice([{ product: "Milk", quantity: 1, price: 1.5 }])); // Output: Newt Amount = 1.5
+// console.log(
+//   getTotalPrice([
+//     { product: "Milk", quantity: 1, price: 1.5 },
+//     { product: "Eggs", quantity: 12, price: 0.1 },
+//     { product: "Bread", quantity: 2, price: 1.6 },
+//     { product: "Cheese", quantity: 1, price: 4.5 },
+//   ])
+// ); // Output: Newt Amount = 10.4
 
 //////////////////////////////////  Q10  ////////////////////////////////////////////
 // Q10. Weekly Salary
@@ -213,18 +213,237 @@ console.log(
 function weeklySalary(hourArr) {
   let salary = 0;
   for (let hour in hourArr) {
-    if (hourArr[hour] <= 8) {
-      salary += hourArr[hour] * 10;
-      // console.log(salary);
-    } else if (hourArr[hour] > 8) {
-      salary += (hourArr[hour] - 8) * 15 + 8 * 10;
-    } else {
-      salary = salary;
+    if (hourArr[hour] != 0) {
+      if (hour == 5 || hour == 6) {
+        salary += (hourArr[hour] - 8) * 30 + 8 * 20;
+      } else {
+        if (hourArr[hour] <= 8) {
+          salary += hourArr[hour] * 10;
+        } else if (hourArr[hour] > 8) {
+          salary += (hourArr[hour] - 8) * 15 + 8 * 10;
+        } else {
+          salary = salary;
+        }
+      }
     }
   }
   return salary;
 }
 
-console.log(weeklySalary([8, 8, 8, 8, 8, 0, 0])); // Output: 400
-console.log(weeklySalary([10, 10, 10, 0, 8, 0, 0])); // Output: 410
-console.log(weeklySalary([0, 0, 0, 0, 0, 12, 0]));
+// console.log(weeklySalary([8, 8, 8, 8, 8, 0, 0])); // Output: 400
+// console.log(weeklySalary([10, 10, 10, 0, 8, 0, 0])); // Output: 410
+// console.log(weeklySalary([0, 0, 0, 0, 0, 12, 0])); // Output: 280
+
+//////////////////////////////////  Q11  ////////////////////////////////////////////
+// Given an object with students and the grades that they made on the tests that they took,
+// determine which student has the best Test Average.
+// The key will be the student's name and the value will be an array of their grades.
+// You will only have to return the student's name.
+// You do not need to return their Test Average.
+
+// Examples
+// getBestStudent({
+// John: [100, 90, 80],
+// Bob: [100, 70, 80]
+// }) ➞ "John"
+// // John's avg = 90
+// // Bob's avg = 83.33
+// getBestStudent({
+// Susan: [67, 84, 75, 63],
+// Mike: [87, 98, 64, 71],
+// Jim: [90, 58, 73, 86]
+// }) ➞ "Mike"
+// Notes
+// All students in an object will have the same amount of test scores. So no student will have taken more tests than another.
+
+function getStudentWithBestTestAvg(studObj) {
+  let maxScore = 0;
+  let avgScore = 0;
+  let studName = "";
+  for (let stud in studObj) {
+    for (let score in studObj[stud]) {
+      avgScore += studObj[stud][score] / studObj[stud].length;
+    }
+    console.log(avgScore);
+    if (avgScore > maxScore) {
+      maxScore = avgScore;
+      studName = stud;
+      // return `${stud} has the best avg test score`;
+    }
+    avgScore = 0;
+  }
+  return `${studName} has the best avg test score`;
+}
+// console.log(
+//   getStudentWithBestTestAvg({
+//     John: [100, 90, 80],
+//     Bob: [100, 70, 80],
+//   })
+// ); // Output: John has the best avg test score
+// console.log(
+//   getStudentWithBestTestAvg({
+//     Susan: [67, 84, 75, 63],
+//     Mike: [87, 98, 64, 71],
+//     Jim: [90, 58, 73, 86],
+//   })
+// ); // Output: John has the best avg test score
+
+//////////////////////////////////  Q12  ////////////////////////////////////////////
+
+// This challenge is an English twist on the Japanese word game Shiritori.
+// The basic premise is to follow two rules:
+// 1. First character of next word must match last character of previous word.
+// 2. The word must not have already been said.
+// Below is an example of a Shiritori game:
+// ["word", "dowry", "yodel", "leader", "righteous", "serpent"] // valid!
+// ["motive", "beach"] //invalid! - beach should start with "e"
+// ["hive", "eh", "hive"] //invalid! - "hive" has already been said
+
+class Shiritori {
+  words = [];
+  isGameOver = false;
+  score = 0;
+  timeOut = 10000; // time in milliseconds
+
+  validWordEntry() {
+    document.write(`<h1 style="color: green;">valid word!</h1>`);
+  }
+  gameOverMessage(msg, word) {
+    document.write(`<h1 style="color: red;">${msg}</h1>`);
+  }
+  scoreAndWordsMessage() {
+    document.write(
+      `<h2 style="color: green;">Score = ${this.score}, words = [${this.words}]</h2>`
+    );
+  }
+
+  outOfTimeMessage() {
+    document.write(
+      `<h2 style="color: red;">Game over. You are out of time.</h2>`
+    );
+    this.scoreAndWordsMessage();
+  }
+  play(word) {
+    if (this.words.includes(word)) {
+      this.isGameOver = true;
+      console.log(`Game over! ${word} already said.`);
+      this.gameOverMessage(`Game over! ${word} already said.`);
+      this.scoreAndWordsMessage();
+    } else {
+      if (this.isGameOver) {
+        console.log(`Game is over! Please start a new game.`);
+        this.gameOverMessage(`Game is over! Please start a new game.`);
+        this.scoreAndWordsMessage();
+      } else {
+        if (this.words.length > 0) {
+          if (word.at(0) == this.words[this.words.length - 1].at(-1)) {
+            this.words.push(word);
+            this.score++;
+          } else {
+            this.isGameOver = true;
+            console.log(
+              `Game over! ${word} does not start with ${this.words[
+                this.words.length - 1
+              ].at(-1)}.`
+            );
+            this.gameOverMessage(
+              `Game over! ${word} does not start with ${this.words[
+                this.words.length - 1
+              ].at(-1)}.`
+            );
+            this.scoreAndWordsMessage();
+          }
+        } else {
+          this.words.push(word);
+          this.score++;
+          this.validWordEntry();
+        }
+      }
+    }
+  }
+
+  playOnBrowser() {
+    let word;
+    let lastWord;
+    while (true) {
+      const startTime = Date.now();
+      if (this.words.length > 0) {
+        word = prompt(
+          `Enter a word starting with ${lastWord} with in 10 seconds`
+        ).toLowerCase();
+        if (Date.now() - startTime >= this.timeOut) {
+          this.outOfTimeMessage();
+          break;
+        }
+      } else {
+        word = prompt("Enter a word").toLowerCase();
+      }
+      if (this.words.includes(word)) {
+        this.isGameOver = true;
+        alert(`Game over! ${word} already said.`);
+        this.gameOverMessage(`Game over! ${word} already said.`);
+        this.scoreAndWordsMessage();
+        console.log(`Game over! ${word} already said.`);
+        break;
+      } else {
+        if (this.isGameOver) {
+          console.log(
+            `Game is over! Please start a new game. Score = ${this.score}`
+          );
+          this.gameOverMessage(`Game is over! Please start a new game.`);
+          this.scoreAndWordsMessage();
+        } else {
+          if (this.words.length > 0) {
+            if (word.at(0) == lastWord) {
+              this.words.push(word);
+              lastWord = this.words[this.words.length - 1].at(-1);
+              this.score++;
+            } else {
+              this.isGameOver = true;
+              alert(
+                `Game over! ${word} does not start with ${this.words[
+                  this.words.length - 1
+                ].at(-1)}. Score = ${this.score}`
+              );
+              this.gameOverMessage(
+                `Game over! ${word} does not start with ${this.words[
+                  this.words.length - 1
+                ].at(-1)}.`
+              );
+              this.scoreAndWordsMessage();
+              console.log(
+                `Game over! ${word} does not start with ${this.words[
+                  this.words.length - 1
+                ].at(-1)}. Score = ${this.score}`
+              );
+              break;
+            }
+          } else {
+            this.words.push(word);
+            lastWord = this.words[this.words.length - 1].at(-1);
+            this.score++;
+          }
+        }
+      }
+    }
+  }
+}
+
+let game1 = new Shiritori();
+// game1.play("word");
+// game1.play("dowry");
+// game1.play("yodel");
+// game1.play("leader");
+// game1.play("righteous");
+// game1.play("serpent");
+
+game1.playOnBrowser();
+
+// let game2 = new Shiritori();
+// game2.play("motive");
+// game2.play("beach");
+
+// let game3 = new Shiritori();
+// game2.play("hive");
+// game2.play("eh");
+// game2.play("hive");
